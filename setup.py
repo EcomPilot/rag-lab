bdf65849a11f37405c04e2f33f1357d114a5aeca
xilinshancun <1643023014@qq.com>
2024-08-29 22:57:28 +0800

    ci(set_up): add setup file for pypi

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='targetpilot-rag-lab',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'openai',
+        'requests',
+        'numpy',
+        'pyecharts',
+        'matplotlib',
+        'networkx',
+        'communities',
+        'pandas',
+        'loguru'
+    ],
+    author='targetpilot',
+    author_email='your.email@example.com',
+    description='RAG-LAB is an open-source RAG toolkit supported by Target Pilot, designed to transform the latest RAG concepts into stable and practical engineering tools. The project currently supports GraphRAG and HybridRAG.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/EcomPilot/rag-lab',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
