d67ab6c4f495bf10ec07d9d22b4c6367f4f5daaf
peixiangong <peixiangong@microsoft.com>
2024-08-30 19:25:40 +0800

    remove unnecessary test

--- test.py
+++
@@ -1,7 +0,0 @@
-from raglab.chunk.regex_text_chunker import simple_chunker
-
-filename = "./examples/documents/Gullivers-travels-A-Voyage-to-Lilliput.txt"
-with open(file=filename, encoding='utf-8') as f:
-    entire_document = f.read()
-    chunks = simple_chunker(entire_document)
-    print(chunks)
