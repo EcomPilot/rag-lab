137bcfb6de5035a4c2b9a2a7655a59a2efb08ed1
peixiangong <peixiangong@microsoft.com>
2024-08-30 22:07:58 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='targetpilot-rag-lab',
+    name='raglab2',
     version='0.1.1',
     packages=find_packages(),
+    license='MIT',
     install_requires=[
         'openai',
         'requests',
