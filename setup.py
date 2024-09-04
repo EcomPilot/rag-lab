60d69b73acc6ad5c89038c73a665de21558cd324
peixiangong <peixiangong@microsoft.com>
2024-09-04 20:00:43 +0800

    upgrade pypi pkg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='raglab2',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     license='MIT',
     install_requires=[
